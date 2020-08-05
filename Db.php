9fa227894fbc38ef87160d0e334fe3550ee41581
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-08-05 13:00:47 +0400

    fix

--- Db.php
+++ Db.php
@@ -1,131 +1,178 @@
 <?php
+
 namespace infrajs\db;
 
-use infrajs\once\Once;
 use infrajs\access\Access;
 use infrajs\router\Router;
 use PDO;
 
-class Db {
-	public static $conf=array(
-		"db"=>false,
-		"user"=>"some",
-		"host"=>"host",
-		"database"=>"some",
-		"password"=>"pass",
-		"port"=>"3306"
+class Db
+{
+	public static $conf = array(
+		"db" => false,
+		"user" => "some",
+		"host" => "host",
+		"database" => "some",
+		"password" => "pass",
+		"port" => "3306"
 	);
+	public static $once = array();
 	public static function &cpdo($debug = null)
 	{
-		return Once::exec('Db::pdo', function &($debug) {
-			$conf = Db::$conf;
-			if (is_null($debug)) $debug = Access::debug();
+		$key = 'pdo:';
+		if (isset(Db::$once[$key])) return Db::$once[$key];
 
-			$ans = false;
 
-			if (!$conf['db']) {
-				//if($debug)die('Нет конфига для соединения с базой данных. Нужно добавить запись mysql: '.Load::json_encode($conf['/mysql']));
-				return $ans;
-			}
-			if (!$conf['user']) {
-				//if($debug)die('Не указан пользователь для соединения с базой данных');
-				return $ans;
-			}
-			if (!class_exists('PDO')) {
-				return $ans;
-			}
+		$conf = Db::$conf;
+		if (is_null($debug)) $debug = Access::debug();
 
-			try {
-				$db = new PDO('mysql:host='.$conf['host'].';dbname='.$conf['database'].';port='.$conf['port'], $conf['user'], $conf['password']);
-				$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-				if ($debug) {
-					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				} else {
-					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-				}
+		$ans = false;
 
-					/*array(
-					PDO::ATTR_PERSISTENT => true,
-					PDO::ATTR_ERRMODE => true,
-					PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING 
-				)*/
-				$db->exec('SET CHARACTER SET utf8');
-			} catch (PDOException $e) {
-				//if($debug)throw $e;
+		if (!$conf['db']) {
+			//if($debug)die('Нет конфига для соединения с базой данных. Нужно добавить запись mysql: '.Load::json_encode($conf['/mysql']));
+			return Db::$once[$key] = $ans;
+		}
+		if (!$conf['user']) {
+			//if($debug)die('Не указан пользователь для соединения с базой данных');
+			return Db::$once[$key] = $ans;
+		}
+		if (!class_exists('PDO')) {
+			return Db::$once[$key] = $ans;
+		}
 
-				$db = false;
-				/*if(!$debug){
-					print "Error!: " . Path::toutf($e->getMessage()) . "<br/>";
-					die();
-				}*/
+		try {
+			$db = new PDO('mysql:host=' . $conf['host'] . ';dbname=' . $conf['database'] . ';port=' . $conf['port'], $conf['user'], $conf['password']);
+			$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			if ($debug) {
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} else {
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 			}
 
-			return $db;
-		}, array($debug));
+			/*array(
+				PDO::ATTR_PERSISTENT => true,
+				PDO::ATTR_ERRMODE => true,
+				PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING 
+			)*/
+			$db->exec('SET CHARACTER SET utf8');
+		} catch (PDOException $e) {
+			//if($debug)throw $e;
+
+			$db = false;
+			/*if(!$debug){
+				print "Error!: " . Path::toutf($e->getMessage()) . "<br/>";
+				die();
+			}*/
+		}
+		Db::$once[$key] = &$db;
+		return $db;
 	}
 	public static function &pdo($debug = null)
 	{
-		return Once::exec('Db::pdo', function &($debug) {
-			header('Cache-Control: no-store'); //no-store ключевое слово используемое в infra_cache
-			$conf = Db::$conf;
-			if (is_null($debug)) $debug = Access::debug();
+		header('Cache-Control: no-store'); //no-store ключевое слово используемое в infra_cache
+		$key = 'pdo:';
+		if (isset(Db::$once[$key])) return Db::$once[$key];
+		
+		$conf = Db::$conf;
+		if (is_null($debug)) $debug = Access::debug();
 
-			$ans = false;
+		$ans = false;
 
-			if (!$conf['db']) {
-				//if($debug)die('Нет конфига для соединения с базой данных. Нужно добавить запись mysql: '.Load::json_encode($conf['/mysql']));
-				return $ans;
-			}
-			if (!$conf['user']) {
-				//if($debug)die('Не указан пользователь для соединения с базой данных');
-				return $ans;
-			}
-			if (!class_exists('PDO')) {
-				return $ans;
-			}
+		if (!$conf['db']) {
+			//if($debug)die('Нет конфига для соединения с базой данных. Нужно добавить запись mysql: '.Load::json_encode($conf['/mysql']));
+			return  Db::$once[$key] = $ans;
+		}
+		if (!$conf['user']) {
+			//if($debug)die('Не указан пользователь для соединения с базой данных');
+			return  Db::$once[$key] = $ans;
+		}
+		if (!class_exists('PDO')) {
+			return  Db::$once[$key] = $ans;
+		}
 
-			try {
-				$db = new PDO('mysql:host='.$conf['host'].';dbname='.$conf['database'].';port='.$conf['port'], $conf['user'], $conf['password']);
-				$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-				if ($debug) {
-					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				} else {
-					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-				}
-
-					/*array(
-					PDO::ATTR_PERSISTENT => true,
-					PDO::ATTR_ERRMODE => true,
-					PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING 
-				)*/
-				$db->exec('SET CHARACTER SET utf8');
-			} catch (PDOException $e) {
-				//if($debug)throw $e;
-
-				$db = false;
-				/*if(!$debug){
-					print "Error!: " . Path::toutf($e->getMessage()) . "<br/>";
-					die();
-				}*/
+		try {
+			$db = new PDO('mysql:host=' . $conf['host'] . ';dbname=' . $conf['database'] . ';port=' . $conf['port'], $conf['user'], $conf['password']);
+			$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			if ($debug) {
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} else {
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 			}
 
-			return $db;
-		}, array($debug));
+			/*array(
+				PDO::ATTR_PERSISTENT => true,
+				PDO::ATTR_ERRMODE => true,
+				PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING 
+			)*/
+			$db->exec('SET CHARACTER SET utf8');
+		} catch (PDOException $e) {
+			//if($debug)throw $e;
+
+			$db = false;
+			/*if(!$debug){
+				print "Error!: " . Path::toutf($e->getMessage()) . "<br/>";
+				die();
+			}*/
+		}
+		Db::$once[$key] = &$db;
+		return $db;
 	}
 	public static function stmt($sql)
 	{
-		return Once::exec('Db::stmt', function ($sql) {
-			$db = Db::pdo();
+		$key = 'sql:'.$sql;
+		if (isset(Db::$once[$key])) return Db::$once[$key];
 
-			return $db->prepare($sql);
-		}, array($sql));
+		$db = Db::pdo();
+
+		return Db::$once[$key] = $db->prepare($sql);
+		
 	}
 	public static function cstmt($sql)
 	{
-		return Once::exec('Db::stmt', function ($sql) {
-			$db = Db::cpdo();
+		$key = 'sql:'.$sql;
+		if (isset(Db::$once[$key])) return Db::$once[$key];
+		
+		$db = Db::cpdo();
 
-			return $db->prepare($sql);
-		}, array($sql));
+		return Db::$once[$key] = $db->prepare($sql);
+	}
+	public static function fetch($sql, $args = [])
+	{
+		$stmt = Db::cstmt($sql);
+		$stmt->execute($args);
+		return $stmt->fetch();
+	}
+	public static function col($sql, $args = [])
+	{
+		$stmt = Db::cstmt($sql);
+		$stmt->execute($args);
+		return $stmt->fetchColumn();
+	}
+	public static function lastId($sql, $args = [])
+	{
+		$db = &Db::cpdo();
+		$stmt = Db::cstmt($sql);
+		$stmt->execute($args);
+		return $db->lastInsertId();
+	}
+	public static function fetchto($sql, $name, $args = [])
+	{ //Колонки в аргументах $func
+		$stmt = Db::cstmt($sql);
+		$stmt->execute($args);
+		$list = array();
+		while ($row = $stmt->fetch()) $list[$row[$name]] = $row;
+		return $list;
+	}
+	public static function all($sql, $args = [])
+	{ //Колонки в аргументах $func
+		$stmt = Db::cstmt($sql);
+		$stmt->execute($args);
+		return $stmt->fetchAll();
+	}
+	public static function exec($sql, $args = [])
+	{
+		$stmt = Db::cstmt($sql);
+		$stmt->execute($args);
+		return $stmt->rowCount();
 	}
 }
